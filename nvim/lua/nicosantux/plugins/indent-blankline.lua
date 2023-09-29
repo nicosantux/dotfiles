46b3c8f467f109862fc1050387ad7baaab359823
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-09-29 10:08:54 -0300

    update indent blank line v3

--- nvim/lua/nicosantux/plugins/indent-blankline.lua
+++ nvim/lua/nicosantux/plugins/indent-blankline.lua
@@ -1,11 +1,11 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   config = function()
-    local indent_blankline = require("indent_blankline")
+    local indent_blankline = require("ibl")
 
     indent_blankline.setup({
       space_char_blankline = " ",
       show_current_context = true,
     })
-  end
+  end,
 }
